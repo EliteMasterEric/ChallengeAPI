a0f7cba1ce5a4d5b5b23f9f5234501ca4ff49045
EliteMasterEric <ericmyllyoja@gmail.com>
2025-09-01 03:53:16 -0400

    Remove a spammy log call.

--- scripts/challengeapi/status.lua
+++ scripts/challengeapi/status.lua
@@ -23,7 +23,7 @@ function ChallengeAPI:GetCurrentChallenge()
 
     local challenge = Game().Challenge
     if challenge == 0 then
-        ChallengeAPI.Log("(getCurrentChallenge) Not in challenge!")
+        -- ChallengeAPI.Log("(getCurrentChallenge) Not in challenge!")
         return nil
     end
 
